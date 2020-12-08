bccec39f69d1e67a25d7f21bd402dd10a057bf92
flaviamedici <m.olivares8915@edmail.edcc.edu>
2020-12-08 10:02:08 -0800

    Added content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, ContentCard, FeatureImage } from "../components"
 
 const IndexPage = () => {
     return (
         <Container>
            <FeatureImage />
+           <Content>
+               <ContentCard 
+               date="December, 08 2020"
+               title= "This is my first post"
+               excerpt= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+               slug="/test"
+               />
+           </Content>
         </Container>
     )
 }
